a1583958bf411cd8a51209660c8b4a09b956261b
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-07-02 21:07:02 +0300

    Update LangComponent.jsx

--- src/components/LangComponent.jsx
+++ src/components/LangComponent.jsx
@@ -42,7 +42,7 @@ const LangComponent = () => {
     <div className="relative inline-block text-left" ref={dropdownRef}>
       <button
         type="button"
-        className = {`inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:ouline-nono focus:ring-2 focus:ring-offset-2 focus:indigo-500 ${buttonBgColor}`}
+        className = {`inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:ouline-no focus:ring-2 focus:ring-offset-2 focus:indigo-500 ${buttonBgColor}`}
         onClick={toggleDropdown}
       >
   
