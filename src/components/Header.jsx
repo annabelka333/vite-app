0fb5477e62763fb77e775f7948f7cac5bfb54bdf
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-07-05 23:38:45 +0300

    created header with logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
-import logo from '../assets/logo.png'
+import logo from '../assets/logo-main.png';
 const Header = () => {
-    return (
-        <header className='bg-white'>
-            <div>
-                <img src={logo} alt='Logo' className='h-10'/>
-
-            </div>
+  return (
+    <header className=' bg-gray-100 p-10 '>   
+      <div className='flex justify-center items-center mb-4'>
+        <img src={logo} alt='Logo' className='h-12 max-w-xs ' />
+        <h1 className='  text-4xl font-bold dark:text-white'
+        > Scheduler tracker</h1>
+      </div>
             
     </header>
-)
-}
+  );
+};
 export default Header
