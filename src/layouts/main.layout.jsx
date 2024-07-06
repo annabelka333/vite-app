ea6b58664f31f731fe1ee5402b8c24e8fb8f8b3b
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-07-07 00:34:36 +0300

    created Footer componet and added CC license

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -2,23 +2,22 @@ import { Outlet } from 'react-router-dom';
 import LangComponent from '../components/LangComponent';
 import InfoModal from '../containers/modal/info.modal';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function MainLayout () {
   //TODO: create greeting components, footer 
 
   return (
     <>
-        <Header />
-        <LangComponent />
+      <Header />
+      <LangComponent />
       <main>
         <div className='container'>
           <Outlet/>
         </div>
       </main>
-      <footer>
-        Developed by Anna V.
-      </footer>
-      <InfoModal />
+     <Footer />
+    <InfoModal />
     </>
   );
 }
