1155645649e559f43de8ba5f2b5265cb90c2a6bc
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-07-08 01:04:00 +0300

    Created TextBlock component

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -3,21 +3,28 @@ import LangComponent from '../components/LangComponent';
 import InfoModal from '../containers/modal/info.modal';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import TextBlock from '../components/TextBlock'; 
 
 function MainLayout () {
-  //TODO: create greeting components, footer 
+
 
   return (
     <>
       <Header />
+      <TextBlock
+        title='Schedule tracker'
+        subtitle='Welcome to tracker'
+        text='Follow the next step to get your appointment.'
+      
+      />
       <LangComponent />
       <main>
         <div className='container'>
           <Outlet/>
         </div>
       </main>
-     <Footer />
-    <InfoModal />
+      <Footer />
+      <InfoModal />
     </>
   );
 }
